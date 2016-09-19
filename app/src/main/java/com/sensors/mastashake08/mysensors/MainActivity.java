78a9fa869fcb5a132f4365b15018c1763f6dbeea
Jyrone Parker <jyrone.parker@gmail.com>
2016-09-18 18:53:10 -0700

    Left comment to show git feature

--- app/src/main/java/com/sensors/mastashake08/mysensors/MainActivity.java
+++ app/src/main/java/com/sensors/mastashake08/mysensors/MainActivity.java
@@ -1,17 +1,22 @@
 package com.sensors.mastashake08.mysensors;
 
+import android.app.ListActivity;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     //how we retrieve our phone sensors
     private SensorManager mSensorManager;
+    private ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
         //get  all sensors
         List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         //This is a comment
+        lv = (ListView) findViewById (R.id.list);
+        lv.setAdapter(new ArrayAdapter<Sensor>(this, android.R.layout.simple_list_item_1,  deviceSensors));
     }
 }
